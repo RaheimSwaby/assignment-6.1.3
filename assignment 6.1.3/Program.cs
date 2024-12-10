8edf4521c18dbb6787f05a906a12f5e5ad28dcb4
Rahei <Rahei@RAHEIMSPC>
2024-12-09 20:24:20 -0500

    Add project files.

---
+++ assignment 6.1.3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace assignment_6._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 0, 3, 4, 5, 0 };
+            int[] result = ZeroAtEnd(arr);
+            Console.WriteLine($"array with zeros at the end ");
+            Console.WriteLine(string.Join(", ", result));
+
+
+        }
+
+
+        public static int[] ZeroAtEnd(int[] arr)
+        {
+            if (arr.Length == 0)
+                return arr;
+
+           
+            int index = 0; 
+
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[index] = arr[i];
+                    index++;
+                }
+            }
+
+            
+            while (index < arr.Length)
+            {
+                arr[index] = 0;
+                index++;
+            }
+
+            return arr;
+        }
+    }
+}
